1b1282c8df9aa021fd428a576f94028ae5c72886
Alexandre Afonso <afonsoaaf@gmail.com>
2024-03-15 14:53:55 -0300

    Organizando

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,42 @@ import (
 	"time"
 )
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /assets/*", http.FileServer(http.Dir("./ui/dist")))
+
+	mux.HandleFunc("GET /*", func(w http.ResponseWriter, r *http.Request) {
+		contentType := "text/html"
+		file := "index.html"
+
+		if r.URL.Path == "/favicon.ico" {
+			contentType = "image/x-icon"
+			file = "favicon.ico"
+		}
+
+		w.Header().Add("Content-Type", contentType)
+		w.WriteHeader(http.StatusOK)
+
+		rawFile, _ := os.ReadFile("./ui/dist/" + file)
+		w.Write(rawFile)
+	})
+
+	serverPort := os.Getenv("PORT")
+
+	if serverPort == "" {
+		serverPort = "3000"
+	}
+
+	log.Println("Starting server at port", serverPort)
+
+	err := http.ListenAndServe(":"+serverPort, mux)
+
+	log.Println("Stopping server at", time.Now().Format(time.RFC3339))
+
+	log.Fatal(err)
+}
+
 // func main() {
 // 	router := chi.NewRouter()
 // 	router.Use(middleware.Logger)
@@ -45,39 +81,3 @@ import (
 
 // 	log.Fatal(err)
 // }
-
-func main() {
-	mux := http.NewServeMux()
-
-	mux.Handle("GET /assets/*", http.FileServer(http.Dir("./ui/dist")))
-
-	mux.HandleFunc("GET /*", func(w http.ResponseWriter, r *http.Request) {
-		contentType := "text/html"
-		file := "index.html"
-
-		if r.URL.Path == "/favicon.ico" {
-			contentType = "image/x-icon"
-			file = "favicon.ico"
-		}
-
-		w.Header().Add("Content-Type", contentType)
-		w.WriteHeader(http.StatusOK)
-
-		rawFile, _ := os.ReadFile("./ui/dist/" + file)
-		w.Write(rawFile)
-	})
-
-	serverPort := os.Getenv("PORT")
-
-	if serverPort == "" {
-		serverPort = "3000"
-	}
-
-	log.Println("Starting server at port", serverPort)
-
-	err := http.ListenAndServe(":"+serverPort, mux)
-
-	log.Println("Stopping server at", time.Now().Format(time.RFC3339))
-
-	log.Fatal(err)
-}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module bitbucket.org/xandreafonso/newsletter-app
 
-go 1.22
-
-require github.com/go-chi/chi v1.5.5
+go 1.22.1
