25cd4f235fe73d0a664c328c6c048f9792334851
Alexandre Afonso <afonsoaaf@gmail.com>
2024-03-15 14:46:35 -0300

    Removendo o Chi router

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,53 @@ import (
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
+// func main() {
+// 	router := chi.NewRouter()
+// 	router.Use(middleware.Logger)
+
+// 	// router.Options("/*")
+
+// 	router.Handle("/assets/*", http.FileServer(http.Dir("./ui/dist")))
+
+// 	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+// 		contentType := "text/html"
+// 		file := "index.html"
+
+// 		if r.URL.Path == "/favicon.ico" {
+// 			contentType = "image/x-icon"
+// 			file = "favicon.ico"
+// 		}
+
+// 		w.Header().Add("Content-Type", contentType)
+// 		w.WriteHeader(http.StatusOK)
+
+// 		rawFile, _ := os.ReadFile("./ui/dist/" + file)
+// 		w.Write(rawFile)
+// 	})
+
+// 	serverPort := os.Getenv("PORT")
+
+// 	if serverPort == "" {
+// 		serverPort = "3000"
+// 	}
+
+// 	log.Println("Starting server at port", serverPort)
+
+// 	err := http.ListenAndServe(":"+serverPort, router)
+
+// 	log.Println("Stopping server at", time.Now().Format(time.RFC3339))
+
+// 	log.Fatal(err)
+// }
+
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	mux := http.NewServeMux()
 
-	// router.Options("/*")
+	mux.Handle("GET /assets/*", http.FileServer(http.Dir("./ui/dist")))
 
-	router.Handle("/assets/*", http.FileServer(http.Dir("./ui/dist")))
-
-	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /*", func(w http.ResponseWriter, r *http.Request) {
 		contentType := "text/html"
 		file := "index.html"
 
@@ -42,7 +75,7 @@ func main() {
 
 	log.Println("Starting server at port", serverPort)
 
-	err := http.ListenAndServe(":"+serverPort, router)
+	err := http.ListenAndServe(":"+serverPort, mux)
 
 	log.Println("Stopping server at", time.Now().Format(time.RFC3339))
 
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module bitbucket.org/xandreafonso/newsletter-app
 
-go 1.20
+go 1.22
 
 require github.com/go-chi/chi v1.5.5
